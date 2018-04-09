3d33e1d2d3f8a963dabe980f00bdc1dc07f36f6a
Gabriel Borcean <gborcean@luc.edu>
2018-04-09 17:26:32 -0500

    Python SUID problem fixed, reduce issue now.

--- scripts/spark.sh
+++ scripts/spark.sh
@@ -5,6 +5,6 @@ module load spark-2.1.1
 
 module show spark-2.1.1
 
-alps-start-master.sh
+alps-start-master.sh #| grep "Starting Spark Master" > ~/apps/pydarshan/scripts/conf
 sleep 120
 alps-start-slaves.sh
